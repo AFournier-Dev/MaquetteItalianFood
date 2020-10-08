043e032488aa50114bbb4f2e0c82944b3af11e18
AFournier-Dev <alexandre.fournier@yahoo.fr>
2020-10-08 15:32:47 +0200

    responsive ok before intageted burger menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,15 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     tileSize: 512,
     zoomOffset: -1,
 }).addTo(mymap);
+
+
+console.log("test");
+const spanMenu=document.getElementById("spanmenu");
+const menuToggle=document.getElementById("menuToggle");
+console.log(spanMenu);
+spanMenu.addEventListener("click",function(e){
+    e.preventDefault();
+    console.log("j'ai cliqué sur le burger");
+    menuToggle.classList.toggle("open");
+});
+
